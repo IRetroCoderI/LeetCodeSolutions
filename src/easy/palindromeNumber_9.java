5d2323c0fe8122f562b2179ed3346ecf5f2109be
Jesus Ramirez <jesusramirez730@gmail.com>
2022-09-12 00:02:31 -0700

    added one solution, pretty proud of it

--- src/easy/palindromeNumber_9.java
+++ src/easy/palindromeNumber_9.java
@@ -1,13 +1,19 @@
 package easy;
 
+import java.util.Arrays;
+
 public class palindromeNumber_9 {
     public static void main(String[] args) {
-        int x = 121;
-        String y = String.valueOf(x);
-        y.charAt(0);
-        System.out.println(y);
+        int num = 123454321;
+        System.out.println(isPalindrome(num));
     }
-    public boolean isPalindrome(int x){
-        return false;
+    static public boolean isPalindrome(int x){
+        String y = String.valueOf(x);
+        char[] reverse = new char[y.length()];
+        for(int i = y.length()-1, j=0; i>=0; i--, j++){
+            reverse[j] = y.charAt(i);    //i goes backwards and j goes forward
+                                        //last value of test number becomes first in array, reversing the string
+        }
+        if(y.equals(String.valueOf(reverse))){ return true; } else { return false; }
     }
 }
